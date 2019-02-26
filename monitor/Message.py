be0b9b7dc92aff32b3cba063fc92360810e1d5bf
davidsean <david.sean@gmail.com>
2019-02-26 11:11:55 -0500

    autopep8

--- monitor/Message.py
+++ monitor/Message.py
@@ -17,11 +17,14 @@ import fcntl
     :synopsis: Incubator module for serial communication between RPi and Arduino
 
 """
+
+
 class ArduinoDirectiveHandler():
     """ Class that handles sending commands to the arduino and verifying
     that the csommands have been accepted and acted uopn by the control
     board.
     """
+
     def __intit__(self):
         ''' Initialization
         Args:
@@ -31,7 +34,7 @@ class ArduinoDirectiveHandler():
 
     def enqueue_parameter_update(self, param, value):
         """ Adds a parameter to the update queue, the updates will be sent
-        at a later time.  If this update follows another requested update, 
+        at a later time.  If this update follows another requested update,
         overwrite it.  The item will remain in the queue until the action
         has been verified
         Args:
@@ -45,7 +48,7 @@ class ArduinoDirectiveHandler():
 
     def get_arduino_command_string(self):
         """ Returns a complete string to send to the arduino in order to
-        update the running parameters.  Commands can be no longer than 80 
+        update the running parameters.  Commands can be no longer than 80
         characters and won't involve removing the command from the queue,
         a separate system will verify that the change has been made.
         Args:
@@ -56,10 +59,12 @@ class ArduinoDirectiveHandler():
             if (len(arduino_command_string) + len(param) + len(str(queuedictionary[param])) + 2) <= 80:
                 if len(arduino_command_string) > 0:
                     arduino_command_string = arduino_command_string + "&"
-                arduino_command_string = arduino_command_string + param + "|" + str(queuedictionary[param])
+                arduino_command_string = arduino_command_string + \
+                    param + "|" + str(queuedictionary[param])
         commandLen = len(arduino_command_string)
         calcCRC = binascii.crc32(arduino_command_string.encode())
-        arduino_command_string = str(commandLen) + "~" + format(calcCRC, 'X') + "$" + arduino_command_string
+        arduino_command_string = str(commandLen) + "~" + format(calcCRC,
+                                                                'X') + "$" + arduino_command_string
 
         return arduino_command_string
 
@@ -111,7 +116,7 @@ class ArduinoDirectiveHandler():
         cmd_string = self.get_arduino_command_string()
         self.clear_queue()
         return cmd_string
-    
+
 
 class Interface():
     """ Class that holds everything important concerning communication.
@@ -144,29 +149,29 @@ class Interface():
         """
 
     def get_serial_number(self):
-      """ Get unique serial number
-      Get a unique serial number from the cpu
-      Args:
-        None
-      """
-      serial = "0000000000"
-      try:
-        with open('/proc/cpuinfo','r') as fp:
-          for line in fp:
-            if line[0:6]=='Serial':
-              serial = line[10:26]
-        if serial == "0000000000":
-          raise TypeError('Could not extract serial from /proc/cpuinfo')
-      except FileExistsError as err :
+        """ Get unique serial number
+        Get a unique serial number from the cpu
+        Args:
+          None
+        """
         serial = "0000000000"
-        print(err.message)
         try:
-            socket.setdefaulttimeout(timeout)
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-            return True
-        except Exception as err:
+            with open('/proc/cpuinfo', 'r') as fp:
+                for line in fp:
+                    if line[0:6] == 'Serial':
+                        serial = line[10:26]
+            if serial == "0000000000":
+                raise TypeError('Could not extract serial from /proc/cpuinfo')
+        except FileExistsError as err:
+            serial = "0000000000"
             print(err.message)
-            return False
+            try:
+                socket.setdefaulttimeout(timeout)
+                socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+                return True
+            except Exception as err:
+                print(err.message)
+                return False
 
     def get_serial_number(self):
         """ Get uniqeu serial number
@@ -278,7 +283,7 @@ class Sensors():
             When there is more than one occurence of char (or when it is not present),
             returns the tuple (False, msg)
         '''
-        
+
         msg = msg.decode().split(char)
         if len(msg) != 2:
             if (self.verbosity == 1):
@@ -296,13 +301,15 @@ class Sensors():
         '''
         # pop the Len
         msg_len, msg = self.pop_param(msg, '~')
-        if msg_len == False: return False
+        if msg_len == False:
+            return False
 
         # pop the Len
         msg_crc, msg = self.pop_param(msg, '$')
-        if msg_crc == False: return False
+        if msg_crc == False:
+            return False
 
-        if string.decode().len()>92:
+        if string.decode().len() > 92:
             if (self.verbosity == 1):
                 print("Length Fail: received string is too long")
             return False
@@ -314,14 +321,14 @@ class Sensors():
             return False
 
         # extract the Len
-        msg_len=lineSplit[0]
+        msg_len = lineSplit[0]
         lineSplit = lineSplit[1].decode().split('$')
         if len(lineSplit) != 2:
             if (self.verbosity == 1):
                 print("Corrupt message: while splitting with length special character '$' ")
             return False
         # extract the CRC
-        msg_crc=lineSplit[0]
+        msg_crc = lineSplit[0]
         calcCRC = binascii.crc32(lineSplit[1].encode().rstrip())
         # compare CRC32
         calcCRC = binascii.crc32(msg.rstrip()) & 0xffffffff
@@ -351,7 +358,8 @@ class Sensors():
         # pop the Len and CRC out
         tmp, msg = self.pop_param(msg, '$')
 
-        if tmp == False: return False
+        if tmp == False:
+            return False
         self.sensorframe = {}
         for params in msg.split('&'):
             kvp = params.split("|")
@@ -380,7 +388,6 @@ if __name__ == '__main__':
     test_commandset = True
     if test_commandset:
         arduino_handler = ArduinoDirectiveHandler()
-
 
         del arduino_handler
 
