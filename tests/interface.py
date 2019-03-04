ad8917350eac20de29e8cf1d7c6ea2db35cf7a93
davidsean <david.sean@gmail.com>
2019-03-04 10:50:38 -0500

    debugged, tests work in RPi

--- tests/interface.py
+++ tests/interface.py
@@ -4,32 +4,34 @@ import unittest
 
 
 class TestInterface(unittest.TestCase):
+    iface = monitor.Interface()
 
     @classmethod
     def setUpClass(self):
-        self.iface = monitor.Interface()
+        iface = monitor.Interface()
 
-    def tearDown(self):
-        del self.iface
+    #@classmethod
+    #def tearDown(self):
+    #    del self.iface
 
     def test_serial_number(self):
         # print("Hardware serial number: {}".format(iface.get_serial_number()))
         # this is only valid for the demo unit
-        assertEqual(iface.get_serial_number(), "00000000f8aa31fe")
+        self.assertEqual(self.iface.get_serial_number(), "00000000f8aa31fe")
 
     def test_get_mac_address(self):
         # print("Hardware address (ethernet): {}".format(iface.get_mac_address()))
-        assertEqual(iface.get_mac_address(), "b8:27:eb:ff:64:ab")
+        self.assertEqual(self.iface.get_mac_address(), "b8:27:eb:ff:64:ab")
 
     def test_internet_connection(self):
         # print("Can contact the mothership: {}".format(iface.connects_to_internet()))
-        assertTrue(iface.connects_to_internet())
+        self.assertTrue(self.iface.connects_to_internet())
         # print("Can contact the mothership: {}".format(iface.connects_to_internet(host = '35.183.143.177', port = 80)))
-        assertTrue(iface.connects_to_internet(host='35.183.143.177', port=80))
+        self.assertTrue(self.iface.connects_to_internet(host='35.183.143.177', port=80))
 
     def serial_connection(self):
         # print("Has serial connection with Arduino: {}".format(iface.serial_connection.is_open()))
-        assertTrue(iface.serial_connection.is_open())
+        self.unittest.assertTrue(self.iface.serial_connection.is_open())
 
 
 if __name__ == '__main__':
